051919fa51fafc61a63407550429ef81e2b1999f
OnlySmene <69465540+OnlySmene@users.noreply.github.com>
2021-02-17 23:50:12 +0100

    Update handleRequests.js

--- scripts/handleRequests.js
+++ scripts/handleRequests.js
@@ -3,13 +3,9 @@ const fetchMatches = async (username) => {
     `https://fortnite-api.com/v1/stats/br/v2?name=${username}`
   );
   const json = await req.json();
-  try {
-    return req.status === 200
-      ? { [username]: json.data.stats.all.overall.matches }
-      : { [username]: null };
-  } catch (err) {
-    return { [username]: null };
-  }
+  return req.status === 200
+    ? { [username]: json.data.stats.all.overall.matches }
+    : { [username]: null };
 };
 
 export default fetchMatches;
